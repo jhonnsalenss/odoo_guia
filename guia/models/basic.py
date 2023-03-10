eb25f19eb7c28c24302f06982c05000a6535ce1e
Jhonn <jhonnsalenss@gmail.com>
2023-03-10 15:06:56 -0400

    Cap4 agregar _description y campos

--- guia/models/basic.py
+++ guia/models/basic.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
 
-from odoo import models
+from odoo import fields, models
 
 class basic(models.Model):
      _name = 'basic.basic'
      _description = 'Basic model test'
+
+     name = fields.Char()
