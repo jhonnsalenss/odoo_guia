ebc95b4e14a3a4789ec64d96dd7f55463d3b0c3d
Jhonn <jhonnsalenss@gmail.com>
2023-03-24 08:23:35 -0400

    Herencias en odoo

--- odoo_inheritance/models/sale_order.py
+++ odoo_inheritance/models/sale_order.py
@@ -8,3 +8,7 @@ class SaleOrder(models.Model):
     _description = 'Herencia de modelo Sale order'
 
     confirmed_user_id = fields.Many2one('res.users', string="Uso de herencia usuario confirmado")
+
+    def action_confirm(self):
+        super(SaleOrder, self).action_confirm
+        self.confirmed_user_id = self.env.user.id
